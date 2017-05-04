8b2a9f6a93936e5024cd76bf7ecb9117f14d5eeb
carlosAldeaInteractive <carlos.glz@aldeainteractive.com>
2017-05-04 11:29:31 -0500

    Creacion de seccion: "Buro de Entidades Financieras"
    Se creo la seccion "Buro de entidades financieras" dentro del directorio
    "buro".

---
+++ buro/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <?php include_once("../include/function.php") ?>
+    <?= getMeta(false); ?>
+    <title>CreeC | Buró de Entidades Financieras</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans" rel="stylesheet">
+    <link href="../style/style.css" rel="stylesheet">
+    <link href="../style/buro.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="../script/script.js"></script>
+    <link href="../responsive/header.css" rel="stylesheet">
+</head>
+<body>
+<?= getHeader(false,''); ?>
+<?php require("../include/contact.php"); ?>
+<?= getBanner("Buró de Entidades Financieras"); ?>
+
+<article id="slide_1" class="background">
+    <article class="margin">
+        <h1>Buró de Entidades Financieras</h1>
+        <div class="block_1">
+            <p>
+                <b>El Buró de Entidades Financieras de la CONDUSEF</b> contiene
+                información de cumplimiento en los registros por parte de ConCreeC,
+                Sociedad Anónima de Capital Variable, Sociedad Financiera de Objeto
+                Múltiple, Entidad No Regulada y FoCreeC, Sociedad Anónima de
+                Capital Variable, Sociedad Financiera de Objeto Múltiple, Entidad
+                No Regulada.
+            </p>
+            <p>
+            Para conocer la información de CreeC, así como de todo el sector de
+            Sociedades Financieras Populares y demás instituciones de crédito,
+            puede acceder al sitio web: http://www.buro.gob.mx.
+            </p>
+            <h2>¿Qué es el Buró de Entidades Financieras?</h2>
+            <p>
+            El Buró de Entidades Financieras es una herramienta de consulta
+            y difusión con la que usted podrá conocer los productos que ofrecen
+            las entidades financieras, sus comisiones y tasas, las reclamaciones
+            de los usuarios, las prácticas no sanas en que incurren, las sanciones
+            administrativas que les han impuesto, las cláusulas abusivas de sus
+            contratos y otra información que resulte relevante para informarle
+            sobre su desempeño.
+            </p>
+        </div>
+        <div class="block_1">
+            <p>
+                Con el Buró de Entidades Financieras usted puede saber quién es
+                quién en bancos, seguros, sociedades financieras de objeto múltiple,
+                cajas de ahorro, afores, entre otras entidades. Con ello, podrá comparar
+                y evaluar a las entidades financieras, sus productos y servicios, y
+                tener mayores elementos para elegir y usar el producto financiero
+                que más le convenga.
+            </p>
+            <p>
+                Esta es una herramienta que puede contribuir al crecimiento
+                económico del país al promover la competencia entre las instituciones
+                financieras. Además, impulsa la transparencia de la información al
+                revelar el desempeño de estas y de los productos que ofrecen.
+                Por otro lado, deriva en un mayor bienestar social al conjugar en un
+                solo espacio información del sistema financiero para darle a usted
+                como usuario más elementos para optimizar su presupuesto, mejorar
+                sus finanzas personales, utilizar correctamente los créditos que
+                fortalecerán su economía y obtener los seguros que la protejan,
+                entre otros aspectos.
+
+            </p>
+        </div>
+    </article>
+</article>
+
+
+
+<?php require("../include/footer.php"); ?>
+</body>
+</html>
