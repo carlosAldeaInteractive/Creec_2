907328b3ef31472d893973c48bf3286136db2e4b
carlosAldeaInteractive <carlos.glz@aldeainteractive.com>
2017-05-05 16:01:02 -0500

    Creacion de seccion: "Costos y comisiones de CreeC"
    Se ha creado la seccion "Costos y comisiones de CreeC" en el directorio
    "costos_comisiones"

---
+++ costos_comisiones/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <?php include_once("../include/function.php") ?>
+    <?= getMeta(false); ?>
+    <title>CreeC | Costos y comisiones</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans" rel="stylesheet">
+    <link href="../style/style.css" rel="stylesheet">
+    <link href="../style/cc.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="../script/script.js"></script>
+    <link href="../responsive/header.css" rel="stylesheet">
+</head>
+<body>
+<?= getHeader(false,''); ?>
+<?php require("../include/contact.php"); ?>
+<?= getBanner("Costos y comisiones de CreeC"); ?>
+
+<article id="slide_1" class="background">
+    <article class="margin">
+        <h1>Costos y comisiones de CreeC</h1>
+        <h2>Ley de transparencia</h2>
+        <p>En CreeC, la transparencia en la información de nuestros productos y servicios es fundamental. Consulta las comisiones de cada uno de ellos.</p>
+        <div class="block_1">
+            <h3>Tarjeta empresarial</h3>
+            <div class="row">
+                <div class="cell"><label>Anualidad</label></div>
+                <div class="cell"><label>$2000</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Manejo de cuenta</label></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Saldo mínimo</label></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Anualidad tarjeta adicional</label></div>
+                <div class="cell"><label>$2000</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Consulta de saldo en cajero ATM</label></div>
+                <div class="cell"><label>La que determine el banco operador</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Disposición de efectivo en cajero ATM</label></div>
+                <div class="cell"><label>La que determine el banco operador</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Monto de retiro de efectivo en cajero ATM por 24 horas</label></div>
+                <div class="cell"><label>$5000</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Pago en ventanillas bancarias en efectivo (48 horas)</label></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Traspasos por transferencia electrónica</label></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Pago tardío</label></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Reposición por robo o extravío</label></div>
+                <div class="cell"><label>$250</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Aclaración improcedente</label></div>
+                <div class="cell"><label>$300</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Servicio de alertas</label></div>
+                <div class="cell"><label>$50</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Compra de tiempo aire (ATM RED®)</label></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Cambio de NIP (ATM RED®)</label></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+        </div>
+        <div class="block_1">
+            <h3>Terminal Punto de Venta Billpocket</h3>
+            <div class="row">
+                <div class="cell"><label>Pago único por adquisición</label></div>
+                <div class="cell"><label>$800</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Renta mensual</label></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Anualidad</></div>
+                <div class="cell"><label>Sin costo</label></div>
+            </div>
+            <div class="row">
+                <div class="cell"><label>Comisión por venta</label></div>
+                <div class="cell"><label>3.5%</label></div>
+            </div>
+            <br>
+            <br>
+            <p>Todas las comisiones son más IVA.</p>
+        </div>
+    </article>
+</article>
+
+
+
+<?php require("../include/footer.php"); ?>
+</body>
+</html>
