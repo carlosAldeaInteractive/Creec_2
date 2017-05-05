d577f03d111675da732cfee455d29a906d50c2a3
carlosAldeaInteractive <carlos.glz@aldeainteractive.com>
2017-05-05 13:12:08 -0500

    Creacion de Seccion: "Derechos ARCO"
    Se creo la seccion "Derechos ARCO" en el directorio "derechos_arco".

---
+++ derechos_arco/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <?php include_once("../include/function.php") ?>
+    <?= getMeta(false); ?>
+    <title>CreeC | Derechos ARCO</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans" rel="stylesheet">
+    <link href="../style/style.css" rel="stylesheet">
+    <link href="../style/da.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="../script/script.js"></script>
+    <link href="../responsive/header.css" rel="stylesheet">
+</head>
+<body>
+<?= getHeader(false,''); ?>
+<?php require("../include/contact.php"); ?>
+<?= getBanner("Derechos ARCO"); ?>
+
+<article id="slide_1" class="background">
+    <article class="margin">
+        <h1>Derechos ARCO</h1>
+        <div class="block_1">
+            <p>
+                Con la finalidad de dar cumplimiento a lo previsto en la Ley Federal
+                de Protección de Datos Personales en Posesión de los Particulares,
+                usted, como titular, tiene derechos que puede ejercer en el uso y
+                disposición de sus datos personales:
+            </p>
+            <p>
+                <b>• Acceso.</b> Derecho a obtener sus datos personales en posesión de
+                CreeC y conocer el Aviso de privacidad al que está sujeto el
+                tratamiento.
+            </p>
+            <p>
+                <b>• Rectificación.</b> Derecho a solicitar que CreeC rectifique sus datos
+                personales cuando estos resulten desactualizados, sean inexactos o
+                estén incompletos.
+            </p>
+            <p>
+                <b>• Cancelación.</b> Derecho a cancelar su información personal cuando
+                considere que no son utilizados conforme a los principios, deberes y
+                obligaciones aplicables.
+            </p>
+            <p>
+                <b>• Oposición.</b> Derecho a oponerse a que sus datos personales sean
+                tratados con fines de publicidad o de prospección comercial, así como
+                otros fines específicos.
+            </p>
+        </div>
+        <div class="block_1">
+            <p>
+                En caso de que requiera revocar el consentimiento o ejercer alguno
+                de los derechos antes mencionados, puede contactarnos
+                directamente al teléfono: (81) 1971 0022.
+            </p>
+            <p>
+                Si el ejercicio de Derechos ARCO se hace por escrito, póngase en
+                contacto con nosotros al correo aviso.privacidad@cree-c.com. Envíe
+                su solicitud a la siguiente dirección: Blr. Díaz Ordaz 140, Torre II,
+                piso 20, Col. Santa María, Monterrey, Nuevo León, México.
+            </p>
+            <p>
+                La solicitud debe ir acompañada con una copia simple de un
+                documento oficial que acredite la identidad del titular o el
+                representante legal, e incluir una descripción de los datos personales
+                sobre los que ejercitará cualquiera de los derechos que le confiere la
+                ley. Asimismo deberá proporcionar un correo electrónico, número
+                telefónico y dirección para recibir notificaciones, a fin de comunicarle
+                la respuesta a su petición y cualquier otra información que facilite la
+                búsqueda y localización de sus datos para los fines requeridos.
+            </p>
+
+        </div>
+    </article>
+</article>
+
+
+
+<?php require("../include/footer.php"); ?>
+</body>
+</html>
